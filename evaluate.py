a0cf6eac05b33ed53d5223b4ef1a90864462a814
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-25 08:17:28 +0900

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -52,7 +52,8 @@ if __name__ == "__main__":
 
     dataset = NERRE_Dataset(sentences, ner_labels, re_labels)
     tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
-    tokenized_data = tokenize_our_data(dataset, tokenizer)
+    tokenized_data = tokenize_our_data(dataset, tokenizer, ner_label2idx, re_label2idx)
+
     
     # Load the saved model's state_dict and configuration
     checkpoint = torch.load(model_path)
