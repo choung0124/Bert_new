637d8915ff1c1c879d4329d1818a862ea8327723
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-25 09:09:19 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -2,13 +2,14 @@ import torch
 from torch.utils.data import Dataset
 
 class NERRE_Dataset(Dataset):
-    def __init__(self, sentences, ner_labels, re_labels):
+    def __init__(self, sentences, subject_labels, object_labels, re_labels):
         self.sentences = sentences
-        self.ner_labels = ner_labels
+        self.subject_labels = subject_labels
+        self.object_labels = object_labels
         self.re_labels = re_labels
 
     def __len__(self):
         return len(self.sentences)
 
-    def __getitem__(self, idx):
-        return self.sentences[idx], self.ner_labels[idx], self.re_labels[idx]
+    def __getitem__(self, index):
+        return self.sentences[index], self.subject_labels[index], self.object_labels[index], self.re_labels[index]
