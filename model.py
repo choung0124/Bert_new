50d867791edf10a24e358cfa0a6f5d74c471f37f
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-25 09:43:52 +0900

    Update model.py

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ class NER_RE_Model(nn.Module):
         self.bert = BertModel.from_pretrained('bert-base-uncased')
         self.subject_ner_classifier = nn.Linear(768, ner_dim)  # Separate classifier for subject NER
         self.object_ner_classifier = nn.Linear(768, ner_dim)  # Separate classifier for object NER
+        self.regular_ner_classifier = nn.Linear(768, ner_dim)  # Additional classifier for regular NER
         self.re_classifier = nn.Linear(768, re_dim)
 
     def forward(self, input_ids, attention_mask, token_type_ids):
@@ -15,5 +16,6 @@ class NER_RE_Model(nn.Module):
         cls_token_output = bert_output.last_hidden_state[:, 0, :]
         ner_logits_subject = self.subject_ner_classifier(cls_token_output)
         ner_logits_object = self.object_ner_classifier(cls_token_output)
+        ner_logits_regular = self.regular_ner_classifier(cls_token_output)  # Get logits for regular NER
         re_logits = self.re_classifier(cls_token_output)
-        return ner_logits_subject, ner_logits_object, re_logits
+        return ner_logits_subject, ner_logits_object, ner_logits_regular, re_logits  # Return regular NER logits too
