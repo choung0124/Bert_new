6014e6a8b28dd14d819f0285b376dcc829f4079f
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-25 10:12:38 +0900

    Update model.py

--- model.py
+++ model.py
@@ -14,10 +14,13 @@ class NER_RE_Model(nn.Module):
         bert_output = self.bert(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
         sequence_output = bert_output[0]
 
-        ner_logits_subject = self.subject_ner_classifier(sequence_output).view(-1, self.subject_ner_classifier.out_features)
-        ner_logits_object = self.object_ner_classifier(sequence_output).view(-1, self.object_ner_classifier.out_features)
-        re_logits = self.re_classifier(sequence_output).view(-1, self.re_classifier.out_features)
+        batch_size = input_ids.shape[0]
+
+        ner_logits_subject = self.subject_ner_classifier(sequence_output).view(batch_size, -1, self.subject_ner_classifier.out_features)[:, 0]
+        ner_logits_object = self.object_ner_classifier(sequence_output).view(batch_size, -1, self.object_ner_classifier.out_features)[:, 0]
+        re_logits = self.re_classifier(sequence_output).view(batch_size, -1, self.re_classifier.out_features)[:, 0]
 
         return ner_logits_subject, ner_logits_object, re_logits
+
 
 
