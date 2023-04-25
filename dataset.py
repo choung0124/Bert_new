386a6d6e9521f95292fdebe92d93a6f4eb35ad05
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-25 10:16:38 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -2,14 +2,16 @@ import torch
 from torch.utils.data import Dataset
 
 class NERRE_Dataset(Dataset):
-    def __init__(self, sentences, subject_labels, object_labels, re_labels):
-        self.sentences = sentences
-        self.subject_labels = subject_labels
-        self.object_labels = object_labels
-        self.re_labels = re_labels
+    def __init__(self, dir_path):
+        full_text, full_entities, full_relations = read_json_files(dir_path)
+        entity_sentences, relation_sentences = extract_sentences(full_text, full_entities, full_relations)
+        self.sentences = [item['sentence'] for item in entity_sentences + relation_sentences]
+        self.subject_labels = [None] * len(entity_sentences) + [item['subject'] for item in relation_sentences]
+        self.object_labels = [None] * len(entity_sentences) + [item['object'] for item in relation_sentences]
+        self.re_labels = [None] * len(entity_sentences) + [item['relation'] for item in relation_sentences]
 
     def __len__(self):
         return len(self.sentences)
 
-    def __getitem__(self, index):
-        return self.sentences[index], self.subject_labels[index], self.object_labels[index], self.re_labels[index]
+    def __getitem__(self, idx):
+        return self.sentences[idx], self.subject_labels[idx], self.object_labels[idx], self.re_labels[idx]
