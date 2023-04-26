8411dbab41c3d076e820145f099c5f043bcb79ab
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-26 22:00:13 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import argparse
 from predict import predict_ner, predict_re
 import json
 import logging
+from tqdm import tqdm
+
 logging.getLogger("transformers").setLevel(logging.ERROR)
 
 parser = argparse.ArgumentParser(description='Predict entities and relations from text.')
@@ -13,16 +15,34 @@ text = args.text
 
 with open("models/combined/relation_to_id.json", "r") as f:
     relation_to_id = json.load(f)
-    
+
 with open("models/combined/label_to_id.json", "r") as f:
     label_to_id = json.load(f)
 
-
+print("Predicting entities...")
 entities = predict_ner(text)
-relations = predict_re(text, entities)
+print(f"Found {len(entities)} entities:")
+for entity in entities:
+    print(entity)
 
-print("Entities:")
-print(entities)
-print("\nRelations:")
-print(relations)
-
+print("Predicting relations...")
+relations = []
+for i, entity1 in tqdm(enumerate(entities), desc="Entity pairs"):
+    for j, entity2 in tqdm(enumerate(entities), desc="Entity pairs"):
+        if i != j:
+            subject_id = entity1["entityId"]
+            object_id = entity2["entityId"]
+            subject = entity1["entityName"]
+            obj = entity2["entityName"]
+            relation_name = ""
+            input_text = f"{subject} [SEP] {obj}"
+            input_ids = tokenizer.encode(input_text, add_special_tokens=True, return_tensors="pt")
+            with torch.no_grad():
+                outputs = model(input_ids)
+            prediction = outputs.pooler_output.argmax(-1).item()
+            relation_name = id_to_relation[prediction]
+            relation_data = {"subjectId": subject_id, "objectId": object_id, "subjectName": subject, "objectName": obj, "relationName": relation_name}
+            relations.append(relation_data)
+print(f"Found {len(relations)} relations:")
+for relation in relations:
+    print(relation)
