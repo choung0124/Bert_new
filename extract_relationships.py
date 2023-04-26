d9f79e6a8b5a0ba03de7686414509edcaa0514b0
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-26 12:18:58 +0900

    Update extract_relationships.py

--- extract_relationships.py
+++ extract_relationships.py
@@ -30,13 +30,13 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
 # Define a function to extract relationships from input text
-def extract_relationships(text, max_length=512, batch_size=8):
+def extract_relationships(text):
     # Split the input text into chunks of length max_length
     chunks = []
     for i in range(0, len(text), max_length):
         chunk = text[i:i+max_length]
         chunks.append(chunk)
-    
+
     # Tokenize and align the NER labels for each chunk
     input_ids_list = []
     attention_masks_list = []
@@ -44,27 +44,31 @@ def extract_relationships(text, max_length=512, batch_size=8):
         tokens = tokenizer.encode_plus(chunk, add_special_tokens=True, padding="max_length", truncation=True, max_length=max_length, return_tensors="pt")
         input_ids_list.append(tokens["input_ids"])
         attention_masks_list.append(tokens["attention_mask"])
-    
+
     # Run the model on each chunk separately and concatenate the outputs
     relationships = []
+    ner_labels = []
     for i in range(0, len(chunks), batch_size):
-        input_ids = torch.cat(input_ids_list[i:i+batch_size], dim=0).to(device)
-        attention_masks = torch.cat(attention_masks_list[i:i+batch_size], dim=0).to(device)
+        input_ids = torch.cat(input_ids_list[i:i+batch_size], dim=0)
+        attention_masks = torch.cat(attention_masks_list[i:i+batch_size], dim=0)
         with torch.no_grad():
-            outputs = model(input_ids, attention_mask=attention_masks)
+            outputs = model(input_ids.to(device), attention_mask=attention_masks.to(device))
         ner_logits = outputs[0]
         ner_label_ids = ner_logits.argmax(dim=2)
         for j in range(len(input_ids)):
             ner_tokens = tokenizer.convert_ids_to_tokens(input_ids[j])
-            ner_labels = [ner_labels[label_id] for label_id in ner_label_ids[j].tolist()]
-            entities = extract_entities(ner_tokens, ner_labels)
-            if len(entities) >= 2:
-                # Extract relations between entities
-                for i in range(len(entities)-1):
-                    for j in range(i+1, len(entities)):
-                        relation = extract_relation(text, entities[i], entities[j])
-                        if relation:
-                            relationships.append((entities[i], relation, entities[j]))
+            ner_label = []
+            for label_id in ner_label_ids[j].tolist():
+                ner_label.append(label_list[label_id])
+            ner_labels.append(ner_label)
+    entities = extract_entities(ner_tokens, ner_labels)
+    if len(entities) >= 2:
+        # Extract relations between entities
+        for i in range(len(entities)-1):
+            for j in range(i+1, len(entities)):
+                relation = extract_relation(text, entities[i], entities[j])
+                if relation:
+                    relationships.append((entities[i], relation, entities[j]))
     return relationships
 
 
