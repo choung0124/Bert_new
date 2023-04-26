b236a6378ec8c73a5d43746189c0ed89ffc94999
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-26 16:10:16 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,17 @@
 import argparse
-import re
-from entity_relation_extraction import extract_entities, extract_relations
+from predict import predict_ner, predict_re
 
-# Define a regular expression pattern to match relation phrases between two entities
-relation_pattern = re.compile(r"\[(?P<subject>e\d+)\]\s(?P<relation>.+)\s\[(?P<object>e\d+)\]")
+parser = argparse.ArgumentParser(description='Predict entities and relations from text.')
+parser.add_argument('text', type=str, help='input text')
 
-if __name__ == "__main__":
-    # Parse command-line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("text", help="input text to extract entities and relations from")
-    args = parser.parse_args()
+args = parser.parse_args()
 
-    # Extract entities from input text
-    text, entities = extract_entities(args.text)
+text = args.text
 
-    # Extract relations from input text and identified entities
-    relations = extract_relations(text, entities)
+entities = predict_ner(text)
+relations = predict_re(text, entities)
 
-    # Print identified entities and relations
-    print("Entities:")
-    for entity_id, entity in entities.items():
-        print(f"- {entity['type']}: {entity['name']} ({entity_id})")
-    print("Relations:")
-    for relation in relations:
-        print(f"- {relation['name']}: {relation['subject']['name']} ({relation['subject']['type']}) -> {relation['object']['name']} ({relation['object']['type']})")
+print("Entities:")
+print(entities)
+print("\nRelations:")
+print(relations)
