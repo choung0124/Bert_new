532167546e88f8855ffba9c492afb6f4e78767cf
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-26 11:47:53 +0900

    Update extract_relationships.py

--- extract_relationships.py
+++ extract_relationships.py
@@ -1,58 +1,82 @@
-import os
-import sys
-import json
 import torch
-from torch.utils.data import DataLoader, TensorDataset
-from transformers import BertTokenizer, BertModel, BertPreTrainedModel
-from torch import nn
+from transformers import BertTokenizer, BertForNERAndRE
+import json
+import argparse
 
-from BERT_full_train import BertForNERAndRE, preprocess_ner, preprocess_re
+parser = argparse.ArgumentParser()
+parser.add_argument("--text", type=str, required=True, help="Input text to extract relationships from")
+args = parser.parse_args()
 
-# Load the model, tokenizer, label mappings
+# Load the fine-tuned custom BERT model and tokenizer
 output_dir = "models/combined"
-model = BertForNERAndRE.from_pretrained(output_dir)
 tokenizer = BertTokenizer.from_pretrained(output_dir)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
+model = BertForNERAndRE.from_pretrained(output_dir)
 
-with open(os.path.join(output_dir, "models/combined/label_to_id.json"), "r") as f:
+# Load the label_to_id and relation_to_id mappings
+with open(os.path.join(output_dir, "label_to_id.json"), "r") as f:
     label_to_id = json.load(f)
-    id_to_label = {idx: label for label, idx in label_to_id.items()}
 
-with open(os.path.join(output_dir, "models/combined/relation_to_id.json"), "r") as f:
+with open(os.path.join(output_dir, "relation_to_id.json"), "r") as f:
     relation_to_id = json.load(f)
-    id_to_relation = {idx: relation for relation, idx in relation_to_id.items()}
 
-# Input text from command line argument
-input_text = sys.argv[1]
+# Function to extract relationships from input text
+def extract_relationships(input_text):
+    input_tokens = tokenizer.encode_plus(input_text, add_special_tokens=True, padding="max_length", truncation=True, max_length=512, return_tensors="pt")
+    input_ids = input_tokens["input_ids"].to(device)
+    attention_mask = input_tokens["attention_mask"].to(device)
 
-# Preprocess and tokenize input text
-json_data = {"text": input_text, "entities": [], "relation_info": []}
-ner_data = preprocess_ner(json_data, tokenizer)
-re_data = preprocess_re(json_data)
+    with torch.no_grad():
+        outputs = model(input_ids, attention_mask=attention_mask)
 
-# Prepare input for the model
-ner_tokens, ner_labels_ = zip(*ner_data)
-encoded_ner = tokenizer.encode_plus(ner_tokens, is_split_into_words=True, add_special_tokens=True, padding="max_length", truncation=True, max_length=512, return_tensors="pt")
-input_ids = encoded_ner["input_ids"].to(device)
-attention_mask = encoded_ner["attention_mask"].to(device)
+    ner_logits, re_logits = outputs[1], outputs[2]
+    ner_predictions = torch.argmax(ner_logits, dim=2)[0]
+    re_predictions = torch.argmax(re_logits, dim=1)[0]
 
-# Run model predictions
-with torch.no_grad():
-    ner_logits, re_logits = model(input_ids, attention_mask=attention_mask)
+    # Extract the subject and object entities
+    entities = []
+    entity_text = ""
+    is_entity = False
 
-ner_logits = ner_logits.cpu().detach().numpy()
-re_logits = re_logits.cpu().detach().numpy()
+    for i, (token_id, ner_label_id) in enumerate(zip(input_ids.squeeze(), ner_predictions)):
+        token = tokenizer.convert_ids_to_tokens(token_id.item())
+        ner_label = list(label_to_id.keys())[list(label_to_id.values()).index(ner_label_id.item())]
 
-# Extract NER and RE predictions
-ner_pred = [id_to_label[lbl] for lbl in ner_logits.argmax(axis=-1).flatten().tolist()]
-re_pred = [id_to_relation[lbl] for lbl in re_logits.argmax(axis=-1).flatten().tolist()]
+        if not is_entity and ner_label.startswith("B-"):
+            is_entity = True
+            entity_text = token.replace("##", "")
+        elif is_entity and ner_label.startswith("I-"):
+            entity_text += token.replace("##", "")
+        elif is_entity and not ner_label.startswith("I-"):
+            is_entity = False
+            entities.append((entity_text, ner_label.split("-")[1]))
 
-# Print NER and RE predictions
-print("NER Predictions:")
-for token, label in zip(ner_tokens, ner_pred):
-    print(f"{token}: {label}")
+    # Extract the relations between the subject and object entities
+    relationships = []
+    relation_text = ""
+    is_relation = False
+    subject, obj = None, None
 
-print("\nRE Predictions:")
-for relation in re_pred:
-    print(relation)
+    for i, (token_id, re_label_id) in enumerate(zip(input_ids.squeeze(), re_predictions)):
+        token = tokenizer.convert_ids_to_tokens(token_id.item())
+        re_label = list(relation_to_id.keys())[list(relation_to_id.values()).index(re_label_id.item())]
+
+        if not is_relation and re_label != "NA":
+            is_relation = True
+            relation_text = re_label
+            subject = entities[i][0]
+        elif is_relation and re_label != "NA":
+            relation_text = re_label
+        elif is_relation and re_label == "NA":
+            is_relation = False
+            obj = entities[i][0]
+            relationships.append((subject, relation_text, obj))
+
+    return relationships
+
+# Example usage
+input_text = args.text
+relationships = extract_relationships(input_text)
+
+print("Extracted relationships:")
+for subject, relationship, obj in relationships:
+    print(f"{subject} - {relationship} - {obj}")
