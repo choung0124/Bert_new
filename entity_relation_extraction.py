80f5f5bf9dd57ada86d89b210a1aa9f6d587341a
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-26 16:07:21 +0900

    Update entity_relation_extraction.py

--- entity_relation_extraction.py
+++ entity_relation_extraction.py
@@ -1,37 +1,27 @@
-import re
+import torch
+from transformers import BertTokenizer, BertForNERAndRE
 
-# Define a regular expression pattern to match entity IDs in brackets, e.g. [e1]
-entity_id_pattern = re.compile(r"\[e\d+\]")
+# Load the tokenizer and model from the output directory
+output_dir = "models/combined"
+tokenizer = BertTokenizer.from_pretrained(output_dir)
+model = BertForNERAndRE.from_pretrained(output_dir)
 
-def extract_entities(text):
-    entities = {}
-    # Find all matches of entity ID pattern
-    for entity_id in entity_id_pattern.findall(text):
-        # Remove brackets from entity ID
-        entity_id = entity_id[1:-1]
-        # Replace entity ID in text with a placeholder
-        text = text.replace(entity_id_pattern.sub(f"@{entity_id}@", entity_id), "")
-        # Extract entity type and name from entity ID
-        entity_type, entity_name = entity_id.split("_")
-        entities[entity_id] = {"type": entity_type, "name": entity_name}
-    return text, entities
+# Set the model to evaluation mode
+model.eval()
 
-def extract_relations(text, entities):
-    relations = []
-    # Split text into sentences
-    sentences = re.split(r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s", text)
-    for sentence in sentences:
-        # Find all matches of relation pattern
-        for match in relation_pattern.finditer(sentence):
-            # Extract subject and object entities using entity IDs
-            subject_id = match.group("subject")
-            object_id = match.group("object")
-            # Ignore relations with unknown entities
-            if subject_id not in entities or object_id not in entities:
-                continue
-            # Extract relation name and add relation to list
-            relation_name = match.group("relation")
-            relation = {"subject": entities[subject_id], "object": entities[object_id], "name": relation_name}
-            relations.append(relation)
-    return relations
+# Define a function to predict NER and RE labels
+def predict_labels(text):
+    # Tokenize the input text
+    input_ids = torch.tensor(tokenizer.encode(text, add_special_tokens=True)).unsqueeze(0)
 
+    # Predict NER labels
+    ner_outputs = model(input_ids)[0]
+    ner_labels = torch.argmax(ner_outputs, axis=-1).squeeze(0)
+    ner_labels = [tokenizer.convert_ids_to_tokens(token_id) for token_id in ner_labels]
+
+    # Predict RE labels
+    re_outputs = model(input_ids)[1]
+    re_labels = torch.argmax(re_outputs, axis=-1).squeeze(0)
+    re_labels = tokenizer.convert_ids_to_tokens(re_labels)
+
+    return ner_labels, re_labels
