f8b3a9591fed5e5d0fc46082aac521e5e49776c3
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-26 13:05:20 +0900

    Update extract_relationships.py

--- extract_relationships.py
+++ extract_relationships.py
@@ -17,11 +17,7 @@ with open(f"{args.model_dir}/relation_to_id.json", "r") as f:
     relation_to_id = json.load(f)
 
 # Load the fine-tuned model and tokenizer
-model = BertForNERAndRE.from_pretrained("bert-base-uncased", num_ner_labels=len(label_to_id), num_re_labels=len(relation_to_id))
-
-model.config.num_ner_labels = len(label_to_id)
-model.config.num_re_labels = len(relation_to_id)
-
+model = BertForNERAndRE.from_pretrained(args.model_dir, num_ner_labels=len(label_to_id), num_re_labels=len(relation_to_id))
 tokenizer = BertTokenizer.from_pretrained(args.model_dir)
 
 # Tokenize the input text
@@ -36,31 +32,40 @@ with torch.no_grad():
         "attention_mask": torch.tensor([attention_mask]),
     }
     outputs = model(**inputs)
-    ner_predictions = outputs["ner_logits"].argmax(dim=2).tolist()[0]
-    re_predictions = outputs["re_logits"].argmax(dim=2).tolist()[0]
+    ner_predictions = outputs[1].argmax(dim=2).tolist()[0]
+    re_predictions = outputs[2].argmax(dim=1).tolist()[0]
 
-# Extract the relationships from the predictions
-relationships = []
+# Extract the entities from the NER predictions
+entities = []
 current_entity = None
-current_relation = None
 
-for token, ner_label, re_label in zip(tokens, ner_predictions, re_predictions):
-    ner_label = list(label_to_id.keys())[list(label_to_id.values()).index(ner_label)]
-    if ner_label.startswith("B-"):
-        if current_relation is not None:
-            relationships.append((current_entity, current_relation, entity))
+for token, label in zip(tokens, ner_predictions):
+    label = list(label_to_id.keys())[list(label_to_id.values()).index(label)]
+    if label.startswith("B-"):
+        if current_entity is not None:
+            entities.append(current_entity)
         current_entity = token
-        current_relation = list(relation_to_id.keys())[list(relation_to_id.values()).index(re_label)]
-    elif ner_label.startswith("I-"):
+    elif label.startswith("I-"):
         if current_entity is None:
             current_entity = token
         else:
             current_entity += " " + token
     else:
-        if current_entity is not None and current_relation is not None:
-            relationships.append((current_entity, current_relation, entity))
+        if current_entity is not None:
+            entities.append(current_entity)
         current_entity = None
-        current_relation = None
+
+if current_entity is not None:
+    entities.append(current_entity)
+
+# Extract the relationships from the RE predictions
+relationships = []
+
+for i, relation_id in enumerate(re_predictions):
+    relation_name = list(relation_to_id.keys())[list(relation_to_id.values()).index(relation_id)]
+    subject = entities[i]
+    obj = entities[i+1]
+    relationships.append((subject, relation_name, obj))
 
 # Print the extracted relationships
 for relationship in relationships:
