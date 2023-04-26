73f096f89c904530b2bf17362d4a16a20b2bc252
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-26 12:55:56 +0900

    Update extract_relationships.py

--- extract_relationships.py
+++ extract_relationships.py
@@ -1,71 +1,61 @@
 import argparse
 import json
 import torch
-from transformers import BertTokenizer
-from BERT_full_train import BertForNERAndRE
+from transformers import BertTokenizer, BertForTokenClassification
 
-# Define command line arguments
-parser = argparse.ArgumentParser(description="Extract relationships from text using a fine-tuned BERT model.")
-parser.add_argument("text", type=str, help="Input text to extract relationships from")
-parser.add_argument("model_dir", type=str, help="Directory containing the fine-tuned BERT model and tokenizer")
-parser.add_argument("--device", type=str, default="cpu", help="Device to run the model on. Defaults to 'cpu'")
-
-# Parse command line arguments
+parser = argparse.ArgumentParser(description="Extract relationships from input text")
+parser.add_argument("--model_dir", type=str, default="models/combined", help="Directory containing the fine-tuned model and tokenizer")
+parser.add_argument("--text", type=str, required=True, help="Input text to extract relationships from")
 args = parser.parse_args()
 
-# Load the model and tokenizer
-model = BertForNERAndRE.from_pretrained(args.model_dir)
+# Load the label_to_id and relation_to_id mappings
+with open(f"{args.model_dir}/label_to_id.json", "r") as f:
+    label_to_id = json.load(f)
+
+with open(f"{args.model_dir}/relation_to_id.json", "r") as f:
+    relation_to_id = json.load(f)
+
+# Load the fine-tuned model and tokenizer
+model = BertForTokenClassification.from_pretrained(args.model_dir, num_labels=len(label_to_id), num_re_labels=len(relation_to_id))
 tokenizer = BertTokenizer.from_pretrained(args.model_dir)
 
-# Set the device to run the model on
-device = torch.device(args.device)
-model.to(device)
-
 # Tokenize the input text
-tokens = tokenizer.encode_plus(args.text, add_special_tokens=True, padding="max_length", truncation=True, max_length=512, return_tensors="pt")
-input_ids = tokens["input_ids"].to(device)
-attention_mask = tokens["attention_mask"].to(device)
+tokens = tokenizer.tokenize(args.text)
+input_ids = tokenizer.convert_tokens_to_ids(tokens)
+attention_mask = [1] * len(input_ids)
 
 # Make predictions using the model
-ner_logits, re_logits = model(input_ids=input_ids, attention_mask=attention_mask)
-ner_predictions = torch.argmax(ner_logits, dim=2)
-re_predictions = torch.argmax(re_logits, dim=1)
+with torch.no_grad():
+    inputs = {
+        "input_ids": torch.tensor([input_ids]),
+        "attention_mask": torch.tensor([attention_mask]),
+    }
+    outputs = model(**inputs)
+    predictions = outputs.logits.argmax(dim=2).tolist()[0]
 
-# Decode the predictions
-ner_labels = tokenizer.convert_ids_to_tokens(ner_predictions[0])
-re_label = next(key for key, value in model.relation_to_id.items() if value == re_predictions[0].item())
-
-# Extract relationships from the NER labels and RE relation label
+# Extract the relationships from the predictions
 relationships = []
-current_subject = None
+current_entity = None
 current_relation = None
-for i in range(len(ner_labels)):
-    label = ner_labels[i]
+
+for token, label in zip(tokens, predictions):
+    label = list(label_to_id.keys())[list(label_to_id.values()).index(label)]
     if label.startswith("B-"):
-        entity_type = label.split("-")[1]
-        current_subject = tokenizer.convert_tokens_to_string([tokenizer.ids_to_tokens(input_ids[0][i].item())])
-        current_relation = None
+        if current_relation is not None:
+            relationships.append((current_entity, current_relation, entity))
+        current_entity = token
+        current_relation = label.split("-")[1]
     elif label.startswith("I-"):
-        entity_type = label.split("-")[1]
-        current_subject += " " + tokenizer.convert_tokens_to_string([tokenizer.ids_to_tokens(input_ids[0][i].item())])
+        if current_entity is None:
+            current_entity = token
+        else:
+            current_entity += " " + token
     else:
-        entity_type = "O"
-        if current_subject is not None:
-            if current_relation is None and entity_type == "O":
-                continue
-            if current_relation is None:
-                current_relation = current_subject + " " + re_label
-            else:
-                obj = tokenizer.convert_tokens_to_string([tokenizer.ids_to_tokens(input_ids[0][i-1].item())])
-                current_relation += " " + obj
-                relationships.append(current_relation)
-                current_relation = None
-            current_subject = None
+        if current_entity is not None and current_relation is not None:
+            relationships.append((current_entity, current_relation, entity))
+        current_entity = None
+        current_relation = None
 
 # Print the extracted relationships
-if len(relationships) == 0:
-    print("No relationships found in the input text.")
-else:
-    print("Extracted relationships:")
-    for relationship in relationships:
-        print(relationship)
+for relationship in relationships:
+    print(relationship)
