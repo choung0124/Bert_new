343c6e11302cad9133a2d5fff95b2c9b8f8a9639
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-29 11:13:31 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,54 +1,57 @@
-import argparse
-from predict import tokenizer, model, id_to_relation, predict_ner, predict_re
-import json
-import logging
-from tqdm import tqdm
 import torch
+import logging
+import json
+import os
+from transformers import BertConfig, BertTokenizerFast
+from BERT_train import BertForNERAndRE  # Assuming the model is defined in a separate file called 'model.py'
 
 logging.getLogger("transformers").setLevel(logging.ERROR)
 
-parser = argparse.ArgumentParser(description='Predict entities and relations from text.')
-parser.add_argument('text', type=str, help='input text')
-
-args = parser.parse_args()
-
-text = args.text
-
-with open("models/combined/relation_to_id.json", "r") as f:
-    relation_to_id = json.load(f)
-
-with open("models/combined/label_to_id.json", "r") as f:
+output_dir = "models/combined"
+with open(os.path.join(output_dir, "label_to_id.json"), "r") as f:
     label_to_id = json.load(f)
 
-print("Predicting entities...")
-entities = predict_ner(text)
-print(f"Found {len(entities)} entities:")
-for entity in entities:
-    print(entity)
+with open(os.path.join(output_dir, "relation_to_id.json"), "r") as f:
+    relation_to_id = json.load(f)
 
-print("Predicting relations...")
-relations = []
-for i, entity1 in tqdm(enumerate(entities), desc="Entity pairs"):
-    for j, entity2 in tqdm(enumerate(entities), desc="Entity pairs"):
-        if i != j:
-            subject_id = entity1["entityId"]
-            object_id = entity2["entityId"]
-            subject = entity1["entityName"]
-            obj = entity2["entityName"]
-            relation_name = ""
-            input_text = f"{subject} [SEP] {obj}"
-            input_ids = tokenizer.encode(input_text, add_special_tokens=True, return_tensors="pt")
-            with torch.no_grad():
-                outputs = model(input_ids)
-            prediction = outputs.pooler_output.argmax(-1).item()
-            if prediction in id_to_relation:
-                relation_name = id_to_relation[prediction]
-                relation_data = {"subjectId": subject_id, "objectId": object_id, "subjectName": subject, "objectName": obj, "relationName": relation_name}
-                relations.append(relation_data)
-            else:
-                relation_name = "unknown"
-                
-print(f"Found {len(relations)} relations:")
-for relation in relations:
-    print(relation)
+config = BertConfig.from_pretrained("bert-base-uncased")
+tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
 
+num_ner_labels = len(label_to_id)
+num_re_labels = len(relation_to_id)
+model = BertForNERAndRE(config, num_ner_labels, num_re_labels)
+
+model_path = "path/to/your/trained/model"  # Replace with the path to your trained model
+model.load_state_dict(torch.load(model_path))
+model.eval()
+
+def extract_relationships(text, model, tokenizer, id_to_label, id_to_relation):
+    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
+
+    # Move inputs to the same device as the model
+    inputs = {k: v.to(model.device) for k, v in inputs.items()}
+
+    # Run the model on the input text
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    # Get the predicted NER and RE labels
+    ner_predictions = torch.argmax(outputs["ner_logits"], dim=-1).squeeze().tolist()
+    re_predictions = torch.argmax(outputs["re_logits"], dim=-1).squeeze().tolist()
+
+    # Convert the predicted labels to their corresponding entity and relationship names
+    ner_labels = [id_to_label[str(pred)] for pred in ner_predictions]
+    re_labels = [[id_to_relation[str(pred)] for pred in row] for row in re_predictions]
+
+    # Locate and print the subject and object entities along with their relationships
+    for i, row in enumerate(re_labels):
+        for j, relation in enumerate(row):
+            if relation != "no_relation":  # Change this to the name of the "no relation" label in your dataset
+                subject = ner_labels[i]
+                object = ner_labels[j]
+                print(f"{subject} entity: {object} entity: {relation}")
+
+    return ner_labels, re_labels
+
+input_text = "Your input text goes here"
+ner_labels, re_labels = extract_relationships(input_text, model, tokenizer, label_to_id, relation_to_id)
