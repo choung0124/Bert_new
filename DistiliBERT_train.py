94597156fe115246eb25828067b3384314ca3d44
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-30 01:15:42 +0900

    Update DistiliBERT_train.py

--- DistiliBERT_train.py
+++ DistiliBERT_train.py
@@ -124,19 +124,16 @@ def custom_collate_fn(batch, max_length):
         except KeyError:
             print("Skipping problematic data")
             continue
-            
-    input_ids = pad_sequence([item['input_ids'] for item in valid_batch], batch_first=True, padding_value=0)
-    attention_mask = pad_sequence([item['attention_mask'] for item in valid_batch], batch_first=True, padding_value=0)
+    
+    try:
+        input_ids = pad_sequence([item['input_ids'] for item in valid_batch], batch_first=True, padding_value=0)
+        attention_mask = pad_sequence([item['attention_mask'] for item in valid_batch], batch_first=True, padding_value=0)
+    except RuntimeError:
+        print("Skipping problematic data during padding")
+        return None
 
     ner_labels = [item['ner_labels'] for item in valid_batch]
-    re_labels = [item['re_labels'] for item in valid_batch]
 
-    return {
-        'input_ids': input_ids,
-        'attention_mask': attention_mask,
-        'ner_labels': ner_labels,
-        're_labels': re_labels
-    }
 
 
 max_length = 128
