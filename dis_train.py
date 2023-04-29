136289334b84caf4cebe6f600f163cbc3e68443c
choung0124 <89881256+choung0124@users.noreply.github.com>
2023-04-29 14:59:58 +0900

    Update dis_train.py

--- dis_train.py
+++ dis_train.py
@@ -3,6 +3,7 @@ import json
 import socket
 import torch
 import torch.distributed as dist
+import argparse
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
@@ -13,65 +14,78 @@ def get_unused_port():
         s.bind(('', 0))
         return s.getsockname()[1]
 
-os.environ['MASTER_ADDR'] = '192.168.100.60'
-os.environ['MASTER_PORT'] = str(get_unused_port())
-os.environ['RANK'] = '0'
-os.environ['WORLD_SIZE'] = '2'
-os.environ['LOCAL_RANK'] = '0'
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Distributed training script")
+    parser.add_argument("--rank", type=int, required=True, help="Rank of the current process")
+    parser.add_argument("--world_size", type=int, required=True, help="Total number of processes")
+    parser.add_argument("--local_rank", type=int, required=True, help="Local rank of the current process")
+    args = parser.parse_args()
+    return args
 
-# Rest of the script remains the same
+def main():
+    args = parse_arguments()
 
+    os.environ['MASTER_ADDR'] = '192.168.100.60'
+    os.environ['MASTER_PORT'] = str(get_unused_port())
+    os.environ['RANK'] = str(args.rank)
+    os.environ['WORLD_SIZE'] = str(args.world_size)
+    os.environ['LOCAL_RANK'] = str(args.local_rank)
 
-# Load and preprocess data
-# Load and preprocess data
-preprocessed_data = []
+    local_rank = int(os.environ['LOCAL_RANK'])
+    torch.cuda.set_device(local_rank)
 
-json_directory = "test"  # Define the directory containing the JSON files
+    # Load and preprocess data
+    preprocessed_data = []
 
-for filename in os.listdir(json_directory):
-    if filename.endswith(".json"):
-        with open(os.path.join(json_directory, filename), "r") as f:
-            json_data = json.load(f)
-            if validate_json(json_data):
-                preprocessed_data.extend(preprocess_data(json_data, tokenizer, label_to_id, relation_to_id))
+    json_directory = "test"
 
+    for filename in os.listdir(json_directory):
+        if filename.endswith(".json"):
+            with open(os.path.join(json_directory, filename), "r") as f:
+                json_data = json.load(f)
+                if validate_json(json_data):
+                    preprocessed_data.extend(preprocess_data(json_data, tokenizer, label_to_id, relation_to_id))
 
+    # Create the dataset
+    dataset = NERRE_Dataset(preprocessed_data, tokenizer, max_length=512, label_to_id=label_to_id, relation_to_id=relation_to_id)
 
-# Create the dataset
-dataset = NERRE_Dataset(preprocessed_data, tokenizer, max_length=512, label_to_id=label_to_id, relation_to_id=relation_to_id)
+    # Initialize the distributed environment
+    torch.cuda.set_device(local_rank)
+    dist.init_process_group(backend='nccl')
 
-# Initialize the distributed environment
-torch.cuda.set_device(local_rank)
-dist.init_process_group(backend='nccl')
+    # Set the device and rank for the current process
+    device = torch.device("cuda", local_rank)
 
-# Set the device and rank for the current process
-device = torch.device("cuda", local_rank)
+    # Replace the DataLoader with a DistributedSampler
+    batch_size = 32  # Define the batch size
+    sampler = DistributedSampler(dataset, num_replicas=int(os.environ['WORLD_SIZE']), rank=local_rank)
+    dataloader = DataLoader(dataset, batch_size=batch_size, sampler=sampler, collate_fn=custom_collate_fn, num_workers=num_workers)
 
-# Replace the DataLoader with a DistributedSampler
-sampler = DistributedSampler(dataset, num_replicas=int(os.environ['WORLD_SIZE']), rank=local_rank)
-dataloader = DataLoader(dataset, batch_size=batch_size, sampler=sampler, collate_fn=custom_collate_fn, num_workers=num_workers)
+    # Wrap the model with DDP
+    model = BertForNERAndRE(config, num_ner_labels, num_re_labels)
+    model = model.to(device)
+    model = DDP(model, device_ids=[local_rank], output_device=local_rank)
 
-# Wrap the model with DDP
-model = BertForNERAndRE(config, num_ner_labels, num_re_labels)
-model = model.to(device)
-model = DDP(model, device_ids=[local_rank], output_device=local_rank)
+    # Call the train function from train.py within try-finally block
+    accumulation_steps = 1  # Define the number of accumulation steps
+    try:
+        model = train(model, dataloader, device, num_epochs, learning_rate, accumulation_steps)
+    finally:
+        dist.destroy_process_group()
 
-# Call the train function from train.py within try-finally block
-try:
-    model = train(model, dataloader, device, num_epochs, learning_rate, accumulation_steps)
-finally:
-    dist.destroy_process_group()
+    # Save the model only in the main process
+    if dist.get_rank() == 0:
+        output_dir = "models/combined"
+        os.makedirs(output_dir, exist_ok=True)
+        model.module.save_pretrained(output_dir)  # Use model.module to access the underlying model
+        tokenizer.save_pretrained(output_dir)
 
-# Save the model only in the main process
-if dist.get_rank() == 0:
-    output_dir = "models/combined"
-    os.makedirs(output_dir, exist_ok=True)
-    model.module.save_pretrained(output_dir)  # Use model.module to access the underlying model
-    tokenizer.save_pretrained(output_dir)
+        # Save the label_to_id and relation_to_id mappings
+        with open(os.path.join(output_dir, "label_to_id.json"), "w") as f:
+            json.dump(label_to_id, f)
 
-    # Save the label_to_id and relation_to_id mappings
-    with open(os.path.join(output_dir, "label_to_id.json"), "w") as f:
-        json.dump(label_to_id, f)
+        with open(os.path.join(output_dir, "relation_to_id.json"), "w") as f:
+            json.dump(relation_to_id, f)
 
-    with open(os.path.join(output_dir, "relation_to_id.json"), "w") as f:
-        json.dump(relation_to_id, f)
+if __name__ == "__main__":
+    main()
